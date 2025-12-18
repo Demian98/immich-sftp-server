c7e644a951916105beebc7b90c3feedf2a30c941
Demian98 <77210091+Demian98@users.noreply.github.com>
2025-12-18 11:21:39 +0100

    better host key handling

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-// src/config.ts
+import fs from 'fs';
 
 function requireEnv(name: string): string {
   const val = process.env[name];
@@ -8,7 +8,25 @@ function requireEnv(name: string): string {
   return val;
 }
 
+function getEnvOrDefault(name: string, defaultValue: string): string {
+  const val = process.env[name];
+  if (!val) {
+    throw new Error(`Missing required environment variable: ${name}`);
+  }
+  return val;
+}
+
+function isRunningInDocker(): boolean {
+  // häufigster Indikator
+  try {
+    if (fs.existsSync("/.dockerenv")) return true;
+  } catch {}
+
+  return false;
+}
+
 export const config = {
   immichHost: requireEnv('IMMICH_HOST'),
   TZ: requireEnv('TZ'),
+  hostKeyDir: getEnvOrDefault('HOST_KEY_DIR', isRunningInDocker() ? '/data/ssh-host-key' : './data/ssh-host-key'),
 };
